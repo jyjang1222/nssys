9f006ff33617a1a63d026edacc52995e2960a78a
jyjang1222 <89000811+jyjang1222@users.noreply.github.com>
2022-03-03 11:52:45 +0900

    Add files via upload

--- PAYDA_New/html/js/main.js
+++ PAYDA_New/html/js/main.js
@@ -1,4 +1,31 @@
 $(function() {
+  $('div[class*="tab-style"] li').click(function() {
+    var idx = $(this).index() + 1;
+
+    $(this).siblings().removeClass('on')
+    $(this).addClass('on');
+
+    $('.tab_cont').hide();
+    $('.tab_cont:nth-child(' + idx + ')').show();
+  }); //탭메뉴
+
+  $('.history_wrap .history_type li').click(function() {
+    $(this).siblings().removeClass('on');
+    $(this).addClass('on');
+  }); //내역 거래유형탭 선택
+
+  $('.btn_modal_close').click(function() {
+    $('#modalPop').hide();
+    $('#modalPop .modal_wrap').hide();
+  }); //계좌생성 약관동의 모달창 닫기
+
+  $('.agree_list a').click(function() {
+    var idx = $(this).parents('li').index() + 1;
+    // console.log(idx);
+    $('#modalPop').show();
+    $('#modalPop .modal_wrap:nth-child(' + idx + ')').show();
+  }) //약관 모달
+
   $('.coin_wrap .coin').click(function() {
     var w = $(window).width();
 
@@ -17,33 +44,6 @@ $(function() {
     $(this).parent().find('.coin_link').toggleClass('on');
     $(this).parent().find('.chevron').toggleClass('on');
   }); //코인메뉴 열기
-
-  $('.send_tab_menu li').click(function() {
-    var idx = $(this).index() + 1;
-
-    $(this).siblings().removeClass('on')
-    $(this).addClass('on');
-
-    $('.send_money_wrap .tab_content').hide();
-    $('.send_money_wrap .tab_content:nth-child(' + idx + ')').show();
-  }); //송금 탭메뉴
-
-  $('.history_wrap .history_type li').click(function() {
-    $(this).siblings().removeClass('on');
-    $(this).addClass('on');
-  }); //내역 거래유형탭 선택
-
-  $('.btn_modal_close').click(function() {
-    $('#modalPop').hide();
-    $('#modalPop .modal_wrap').hide();
-  }); //계좌생성 약관동의 모달창 닫기
-
-  $('.agree_list a').click(function() {
-    var idx = $(this).parents('li').index() + 1;
-    // console.log(idx);
-    $('#modalPop').show();
-    $('#modalPop .modal_wrap:nth-child(' + idx + ')').show();
-  }) //약관 모달
 
   $('.coin_send_wrap .tab_menu li').click(function() {
     var idx = $(this).index() + 1;

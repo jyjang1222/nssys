8cec2b2f881c9fadaed69b3473968bd0dea941d9
jyjang1222 <89000811+jyjang1222@users.noreply.github.com>
2022-03-08 16:21:05 +0900

    Add files via upload

--- PAYDA_New/html/js/FnbStyle.js
+++ PAYDA_New/html/js/FnbStyle.js
@@ -5,19 +5,23 @@ switch(true) {
   case url.includes("SimplePay"):
     $('#fnb li:nth-child(1)').addClass('on');
     break;
-    case url.includes("charge"):
+  case url.includes("charge"):
+  $('#fnb li:nth-child(1)').addClass('on');
+    break;
+  case url.includes("SendMoney"):
     $('#fnb li:nth-child(1)').addClass('on');
     break;
-    case url.includes("SendMoney"):
+  case url.includes("ReceiveQrcode"):
     $('#fnb li:nth-child(1)').addClass('on');
     break;
-    case url.includes("ReceiveQrcode"):
-    $('#fnb li:nth-child(1)').addClass('on');
-    break;
-    case url.includes("SimplePayHistory"):
+  case url.includes("SimplePayHistory"):
     $('#fnb li:nth-child(1)').addClass('on');
     break;
   case url.includes("CoinWallet"):
     $('#fnb li:nth-child(2)').addClass('on');
     break;
+  case url.includes("CouponShop"):
+    $('#fnb li:nth-child(3)').addClass('on');
+    break;
+  
 };
